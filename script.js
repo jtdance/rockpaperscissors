e06e97eb4d2a09190e4e76fbec15c96020632dab
Justin Dance <jtdance@icloud.com>
2021-10-15 15:46:31 -0400

    added UI with HTML and CSS

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const myArray = ["rock","paper","scissors"];
-const getUserChoice = prompt("Rock, Paper, or Scissors?");
-
-function computerPlay() {
-    return myArray[Math.floor(Math.random() * myArray.length)]
+const pickUserHand = (hand) => {
+    console.log(hand);
 }
-
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection == computerSelection) {
-        return ("Tie Game!");
-    } else if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "paper" && playerSelection == "rock") ||
-        (computerSelection == "scissors" && playerSelection == "paper")) {
-            return ("You Lose!");
-        } else {
-            return ("You Win!");
-        }
-}
-
-const playerSelection = getUserChoice;
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
